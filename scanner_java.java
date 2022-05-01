d6dc52a670da165faddf210739f30e125cecc306
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-05-01 12:14:22 +0530

    Add files via upload

---
+++ scanner_java.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * scanner_java
+ */
+public class scanner_java {
+
+    public static void main(String[] args) {
+        Scanner stdname = new Scanner(System.in);
+
+        System.out.println("Enter First Name : ");
+        String fname = stdname.nextLine();
+
+        System.out.println("Enter Last Name : ");
+        String lname = stdname.nextLine();
+
+        System.out.println("You Name is : "+fname+" "+lname);
+    }
+}
